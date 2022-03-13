f842bb1988191b7e6f70d0f5f878d5ef6991cc4c
setepenre <setepenre@outlook.com>
2022-03-12 22:46:33 -0500

    Update missing dependency

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ if __name__ == '__main__':
     setup(
         name='ranked',
         version='0.0.0',
-        description='Model Zoo for Ranking algo',
+        description='Player Ranking Algorithm benchmarks',
         author='Pierre Delaunay',
         packages=[
             'ranked',
         ],
-        install_requires=['scipy', 'altair'],
+        install_requires=['scipy', 'altair', 'trueskill',],
         setup_requires=['setuptools'],
     )
