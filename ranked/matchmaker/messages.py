7a5619596d00ef6fc07219afccf4d2770c67923b
Setepenre <pierre.delaunay@outlook.com>
2023-01-29 13:09:36 -0500

    wip

---
+++ ranked/matchmaker/messages.py
@@ -0,0 +1,49 @@
+from dataclasses import dataclass, field
+from enum import Enum
+import secrets
+from typing import Optional
+
+
+class MessageKind(Enum):
+    MMRequest = 1
+    MMResponse = 2
+    MMServerConfig = 3
+    KeepAlive = 4
+
+
+@dataclass
+class MatchmakingRequest:
+    kind: MessageKind
+    player_ids: list[str]   
+    player_skills: list[int] = field(default_factory=list)
+    client_secrets: list[str] = field(default_factory=list)
+    request_id: Optional[int] = None
+
+    def generate_secrets(self):
+        self.client_secrets = [secrets.token_hex(16) for _ in self.player_ids]
+
+    def skill(self):
+        return int(sum(self.player_skills) / len(self.player_skills))
+
+    def player_count(self):
+        return len(self.player_ids)
+
+
+@dataclass
+class KeepAlive:
+    kind: MessageKind = MessageKind.KeepAlive
+
+
+@dataclass
+class MatchmakingResponse:
+    server_ip: str            # Address of the game instance
+    server_port: int          # Port to use to connect to the game instane
+    client_secret: list[str]  # secret to use to connect to the instance
+
+    kind: MessageKind = MessageKind.MMResponse
+
+
+@dataclass
+class MatchmakingServerConfig:
+    kind: MessageKind   
+    client_secrets: list[str]   # expected client secrets 
---
+++ ranked/matchmaker/server.py
@@ -0,0 +1,120 @@
+import asyncio
+import socket
+from struct import unpack
+from dataclasses import asdict
+import json
+import sys
+import logging
+
+import ranked.matchmaker.schema as db
+from ranked.matchmaker.worker import Matchmaker
+from ranked.matchmaker.messages import MessageKind, MatchmakingRequest, MatchmakingResponse
+
+log = logging.getLogger(__name__)
+
+
+
+class Server:
+    def __init__(self) -> None:
+        self.database_uri = ''
+        self.worker = Matchmaker()
+        self._dispatch = {
+            MessageKind.MMRequest: self.matchmaking_request
+        }
+
+        db.create_database(self.get_sql_client())
+
+    def get_sql_client(self):
+        return db.get_sql_engine(self.database_uri)
+
+    async def fetch_player_skills(self, mm_request: MatchmakingRequest):
+        engine = self.get_sql_client()
+        db.fetch_player_skills(engine, mm_request)
+
+    async def find_free_game_server(self):
+        engine = self.get_sql_client()
+        return db.find_game_server(engine)
+
+    def find_team(self, mm_request: MatchmakingRequest, promise: asyncio.Future):
+        pass
+
+    async def matchmaking_request(self, client, payload):
+        mm_request = MatchmakingRequest(**payload)
+        mm_request.generate_secrets() 
+
+        await self.fetch_player_skills(mm_request)
+
+        loop = asyncio.get_event_loop()
+        result = loop.create_future()
+        self.find_team(mm_request, result)
+
+        await result
+
+        server = await self.find_free_game_server()
+
+        await self.send_json(client, asdict(MatchmakingResponse(
+            server.ip,
+            server.port,
+            mm_request.client_secrets,
+        )))
+        client.close()
+
+    async def send_json(self, client, obj):
+        payload = json.dumps(obj).encode('utf8')
+
+        size = len(payload)
+        size_bytes = size.to_bytes(4, byteorder=sys.byteorder)
+
+        loop = asyncio.get_event_loop()
+        await loop.sock_sendall(client, size_bytes)
+        await loop.sock_sendall(client, payload)
+
+    async def handle_request(self, client):
+        loop = asyncio.get_event_loop()
+
+        size_bytes = (await loop.sock_recv(client, 4))
+        size = int(unpack("@I", size_bytes)[0])
+
+        request_bytes = (await loop.sock_recv(client, size))
+        
+        payload = json.loads(request_bytes)
+        
+        handler = self._dispatch.get(MessageKind.MMRequest, None)
+
+        if handler:
+            handler(client, payload)
+
+        client.close()
+
+    async def run_server(self):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        server.bind(('localhost', 15555))
+        server.listen(8)
+        server.setblocking(False)
+
+        loop = asyncio.get_event_loop()
+
+        # the matchmaker worker
+        loop.create_task(self.worker.set_promises())
+
+        # start accepting clients
+        while True:
+            try:
+                client, _ = await loop.sock_accept(server)
+                loop.create_task(self.handle_request(client))
+            except KeyboardInterrupt:
+                log.info("Server stopped by user")
+                break
+
+        self.worker.stop()
+        # task.cancel()
+
+
+def main():
+    mm = Server()
+    asyncio.run(mm.run_server())
+
+
+if __name__ == '__main__':
+    main()
